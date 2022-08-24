9776b55eab05a488cb07e5d2cb2503b50313b5bb
Jonathan Vegas <jonathanvegas@gmail.com>
2022-08-24 15:43:15 -0400

    Deploying React app on AWS

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>React on AWS S3 bucket</h1>
-        <footer>by Jonathan Vegas</footer>
+        <footer>by Jonathan Vegas®</footer>
       </header>
     </div>
   );
